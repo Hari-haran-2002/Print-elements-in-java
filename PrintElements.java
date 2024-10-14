131c59e4db6361c70aff490ff78da1588484e7d8
Hari-haran-2002 <jagahari2002@gmail.com>
2024-10-14 15:27:06 +0530

    Add files via upload

---
+++ PrintElements.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class PrintElements {
+    public static void main(String[] args) {
+        // Create an ArrayList and add some elements
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        list.add("Date");
+        list.add("Elderberry");
+
+        // Print each element using its position (index)
+        System.out.println("Elements of the ArrayList using their position:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("Element at index " + i + ": " + list.get(i));
+        }
+    }
+}
